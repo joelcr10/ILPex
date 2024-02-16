7a9a95d908f1910278189a3831f288a8d5ee5ac7
Ashik George <ashikgeorgec@gmail.com>
2024-02-16 18:05:01 +0530

    Feat : Updated Files

--- src/controllers/getAllUsers.ts
+++ src/controllers/getAllUsers.ts
@@ -1,28 +1,19 @@
 import express,{Request,Response} from 'express';
-import LandDtable from '../models/l&d'
-import BatchTable from '../models/batchTable';
+import LandDtable from '../models/lnd'
+import BatchTable from '../models/trainees';
 const app =express();
 app.use(express.json());
-
 const getUsers = async (req:Request,res:Response) => {
     try{
-    const names = await LandDtable.findAll({attributes:['name','role']})
-    const batches = await BatchTable.findAll({attributes:['name','role']})
-    const landdnames = names.map(users=>({name:users.name,role:users.role}));
-    const batchnames = batches.map(users=>({name:users.name,role:users.role}));
+    const names = await LandDtable.findAll({attributes:['name','user_id']})
+    const batches = await BatchTable.findAll({attributes:['name','user_id']})
+    const landdnames = names.map(users=>({name:users.name,id:users.user_id}));
+    const batchnames = batches.map(users=>({name:users.name,id:users.user_id}));
     const responseData ={
         landdnames:landdnames,
         batchnames:batchnames
     }
     return res.json(responseData);
-    
-
-        // return res.json(names.map(users=>users.name)) 
-    // const respondData = {
-    //     landdnames:names.map(users=>{users.name,users.role}),
-    //     batchnames:batches.map(users=>{users.name,users.role})
-    // }
-    // return res.json(respondData);
     }catch(err){
         return res.json(err);
     }
