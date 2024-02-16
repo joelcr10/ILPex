f19cc4edf34150d764a61e9fab6203d5224cfb07
Nebil V <nebilvxavier@gmail.com>
2024-02-16 15:21:22 +0530

    feat: AllAssesment api

--- types/modelTypes/assessments.ts
+++ types/modelTypes/assessments.ts
@@ -1,15 +1,14 @@
 import { Model } from 'sequelize';
-
+ 
 class assessment extends Model{
     public assessment_id ?:number;
     public assessment_name !: string;
-    public batch_id !: number;
-    public user_id !: number;
     public assessment_date !: Date;
+    public no_of_attempts ?: number;
     public created_on ?: Date;
     public created_by ?: number;
     public modified_on ?: Date;
     public modified_by ?: number;
-  
+ 
   }
   export default assessment ;
