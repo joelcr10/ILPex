e4e6172aa57dc41e925c97c3299c67ba9e64d7e6
ThimnaRaphel <thimnaraphel12@gmail.com>
2024-02-16 15:29:58 +0530

    feat - added assessment_batches_mapping

--- src/models/assessments_batches_mapping.ts
+++ src/models/assessments_batches_mapping.ts
@@ -1,8 +1,8 @@
 import { DataTypes,Sequelize } from 'sequelize';
 import sequelize from '../config/sequelize-config'; 
-import AssessmentsBatchesMapping from '../../types/modelTypes/assessments_batches_mapping';
+import assessments_batches_mapping from '../../types/modelTypes/assessments_batches_mapping';
 
-AssessmentsBatchesMapping.init({
+assessments_batches_mapping.init({
     batches_id : {
         type: DataTypes.INTEGER,
         allowNull: false
@@ -15,4 +15,5 @@ AssessmentsBatchesMapping.init({
     sequelize,
     modelName:"assessments_batches-mapping",
     tableName:"assessments_batches-mapping"
-})
+});
+export default assessments_batches_mapping;
