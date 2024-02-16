f81c3b65a57592f15b0bf8347264e8b1109b0ac7
Sreejaya <itsmesreejaya@gmail.com>
2024-02-16 18:16:23 +0530

    feat:user table created

--- src/controllers/authentication/register.ts
+++ src/controllers/authentication/register.ts
@@ -7,14 +7,14 @@ import Roles from "../../models/roles";
 
 const userRegistration = async(req:Request,res:Response)=>{
     try{
-        const {email, user_name, password, role_id}= req.body;
+        const {email, user_name, password, role_id,created_by_role_id}= req.body;
         if (!email || !user_name || !password || !role_id ) {
             return res.status(400).json({ error: 'All fields are required' });
           }
 
-        //   if (created_by_role_id!== 101) {
-        //     return res.status(403).json({ error: 'Only admin can create users' });
-        // }
+          if (created_by_role_id!== 101) {
+            return res.status(403).json({ error: 'Only admin can create users' });
+        }
         const role_name = await Roles.findOne({where:{role_id:role_id}});
 
         const existingUser = await Users.findOne({where: {email: email}});
