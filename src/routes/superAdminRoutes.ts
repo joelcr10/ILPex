9e9188b09d78c0c50c6f1ea3e0ba45faac636534
joelcr10 <joelcraju@gmail.com>
2024-02-17 15:42:01 +0530

    saving changes

--- src/routes/superAdminRoutes.ts
+++ src/routes/superAdminRoutes.ts
@@ -4,6 +4,7 @@ import getUserList from '../controllers/getAllUsers'
 import createBatchController from '../controllers/lAndDControllers/createBatchController';
 import createCourseController from '../controllers/admin/createCourse';
 import createDaysController from '../controllers/admin/createDaysController';
+import createDaywiseCourseMappingController from '../controllers/admin/createDaywiseCourseMappingController';
 
 // api endpoints related to super admin are put here
 const router = Router();
@@ -31,6 +32,10 @@ router.post('/createCourse', async (req: Request, res: Response) =>{
 //just to create a Day table with day_id and Day_name
 router.post('/createDays', async (req: Request, res: Response) =>{
     await createDaysController(req, res);
+})
+
+router.post('/createCourseMapping', async (req, res) =>{
+    await createDaywiseCourseMappingController(req,res);
 })
 
 export default router;
