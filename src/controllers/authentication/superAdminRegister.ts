9dae6ccda16b56c8e96330be33a9378bf81de37f
Sreejaya <itsmesreejaya@gmail.com>
2024-02-20 18:28:59 +0530

    feat:userLogin and userRegistration API modified

--- src/controllers/authentication/superAdminRegister.ts
+++ src/controllers/authentication/superAdminRegister.ts
@@ -1,8 +1,8 @@
-import express,{ Request,Response } from "express";
-import superAdminRegistration from "../../services/adminServices/superAdminRegistration";
+// import express,{ Request,Response } from "express";
+// import superAdminRegistration from "../../services/adminServices/superAdminRegistration";
 
-const superAdminRegister = async(req:Request,res:Response)=>{
-    superAdminRegistration(req,res);
-}
+// const superAdminRegister = async(req:Request,res:Response)=>{
+//     superAdminRegistration(req,res);
+// }
 
-export default superAdminRegister;
+// export default superAdminRegister;
