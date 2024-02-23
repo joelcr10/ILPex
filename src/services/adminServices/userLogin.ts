043447df923d3a24ac2a217ddca21968ca1d588f
Sreejaya <itsmesreejaya@gmail.com>
2024-02-23 18:42:12 +0530

    feat:jwttokencode stored in .env

--- src/services/adminServices/userLogin.ts
+++ src/services/adminServices/userLogin.ts
@@ -1,73 +1,109 @@
-import express,{Router,Request,Response} from "express";
+import express, { Router, Request, Response } from "express";
 import jwt from "jsonwebtoken";
 import bcrypt from "bcrypt";
 import Users from "../../models/users";
+import dotenv from "dotenv";
 
+dotenv.config();
 
-const userLogin = async(req:Request,res:Response): Promise<
-        Response<
-          any,
-          Record<
-            string,
-            | { message: string }
-            | { token: string; client_type: "string"; registration_id: string }
-          >
-        >
-      > => {
-        const { email, password} = req.body;
-        const userFound = await Users.findOne({
-            where: { email: email },
+const { JWTTOKENCODE } = process.env as { JWTTOKENCODE: string | undefined };
+
+const userLogin = async (
+  req: Request,
+  res: Response
+): Promise<
+  Response<
+    any,
+    Record<
+      string,
+      | { message: string }
+      | { token: string; client_type: "string"; registration_id: string }
+    >
+  >
+> => {
+  const { email, password } = req.body;
+  const userFound = await Users.findOne({
+    where: { email: email },
+  });
+  if (userFound) {
+    if (userFound?.role_id == 101) {
+      if (userFound && bcrypt.compareSync(password, userFound.password)) {
+        if (JWTTOKENCODE && userFound?.user_id && userFound?.role_id) {
+          const token = jwt.sign(
+            { user_reg_id: userFound?.user_id, usertype: userFound?.role_id },
+            JWTTOKENCODE,
+            { expiresIn: "24h" }
+          );
+          return res.status(200).json({
+            message: `SuperAdmin logged in!`,
+            token: ` ${token}`,
+            userid: `${userFound.user_id}`,
           });
-        if(userFound){
-            if (userFound?.role_id == 101){
-                if (userFound && bcrypt.compareSync(password, userFound.password)) {
-                  const token = jwt.sign(
-                    { user_reg_id: userFound?.user_id, usertype:userFound?.role_id },
-                    "verify_login",
-                    { expiresIn: "24h" }
-                  );
-                  return res.status(200).json({message: `SuperAdmin logged in!`, token:` ${token}`, userid:`${userFound.user_id}`});
-                }else{
-                  return res.status(404).json({error:`Incorrect password`});
-                } 
-              }
-              if(userFound?.role_id == 102){
-                  const userFound = await Users.findOne({
-                      where: { email: email },
-                    });
-                    if (userFound && bcrypt.compareSync(password, userFound.password)) {
-                      const token = jwt.sign(
-                        { user_reg_id: userFound?.user_id, usertype:userFound?.role_id },
-                        "verify_login",
-                        { expiresIn: "24h" }
-                      );
-                      return res.status(200).json({message: `l_and_d logged in!`, token:` ${token}`,userid:`${userFound.user_id}`});
-                    }else{
-                      return res.status(404).json({error:`Incorrect password`});
-                    }  
-      
-              }
-              if(userFound?.role_id == 103){
-                  const userFound = await Users.findOne({
-                      where: { email: email },
-                    });
-                    if (userFound && bcrypt.compareSync(password, userFound.password)) {
-                      const token = jwt.sign(
-                        { user_reg_id: userFound?.user_id, usertype:userFound?.role_id },
-                        "verify_login",
-                        { expiresIn: "24h" }
-                      );
-                      return res.status(200).json({message: `Trainee logged in! `, token:` ${token}`,userid:`${userFound.user_id}`});
-                    }else{
-                      return res.status(404).json({error:`Incorrect password`});
-                    }  
-      
-              }
+        } else {
+          console.error(
+            "Unable to sign the token. Check if JWTTOKENCODE and userFound are defined."
+          );
         }
+      } else {
+        return res.status(404).json({ error: `Incorrect password` });
+      }
+    }
+    if (userFound?.role_id == 102) {
+      const userFound = await Users.findOne({
+        where: { email: email },
+      });
+      if (userFound && bcrypt.compareSync(password, userFound.password)) {
 
+        if (JWTTOKENCODE && userFound?.user_id && userFound?.role_id) {
+          const token = jwt.sign(
+            { user_reg_id: userFound?.user_id, usertype: userFound?.role_id },
+            JWTTOKENCODE,
+            { expiresIn: "24h" }
+          );
+          return res.status(200).json({
+          message: `l_and_d logged in!`,
+          token: ` ${token}`,
+          userid: `${userFound.user_id}`,
+          });
+        } else {
+          console.error(
+            "Unable to sign the token. Check if JWTTOKENCODE and userFound are defined."
+          );
+        }
+      } else {
+        return res.status(404).json({ error: `Incorrect password` });
+      }
+    }
+    if (userFound?.role_id == 103) {
+      const userFound = await Users.findOne({
+        where: { email: email },
+      });
+      if (userFound && bcrypt.compareSync(password, userFound.password)) {
+
+        if (JWTTOKENCODE && userFound?.user_id && userFound?.role_id) {
+          const token = jwt.sign(
+            { user_reg_id: userFound?.user_id, usertype: userFound?.role_id },
+            JWTTOKENCODE,
+            { expiresIn: "24h" }
+          );
+          return res.status(200).json({
+          message: `Trainee logged in! `,
+          token: ` ${token}`,
+          userid: `${userFound.user_id}`,
+        });
+        } else {
+          console.error(
+            "Unable to sign the token. Check if JWTTOKENCODE and userFound are defined."
+          );
+        }
         
-        return res.status(404).json("No such usertype found");
-      };
+      } else {
+        return res.status(404).json({ error: `Incorrect password` });
+      }
+    }
+  }
 
+  return res.status(404).json("No such usertype found");
+};
 
 export default userLogin;
