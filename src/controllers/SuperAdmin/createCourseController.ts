bcb0f0c107f61644ef6d6ff03ba7f18cfd70b56c
joelcr10 <joelcraju@gmail.com>
2024-02-24 14:06:59 +0530

    changed Courses table

--- src/controllers/SuperAdmin/createCourseController.ts
+++ src/controllers/SuperAdmin/createCourseController.ts
@@ -6,37 +6,37 @@ import createCourseServices from "../../services/adminServices/createCourseServi
 
 const createCourseController = async (req: Request, res: Response) =>{
     try{
-        const {course_name,course_link, course_type_id, course_duration, day_number,course_date,createdBy} = req.body;
+        const {course_name, course_type, course_duration, day_number,createdBy} = req.body;
 
         if(!course_name){
             return res.status(404).json({message: "invalid course name"});
         }
-        else if(!course_link){
-            return res.status(404).json({message: "invalid course link"});
-        }
-        else if(!course_type_id){
-            return res.status(404).json({message: "invalid course type id"});
-        }
+        // else if(!course_link){
+        //     return res.status(404).json({message: "invalid course link"});
+        // }
+        // else if(!course_type_id){
+        //     return res.status(404).json({message: "invalid course type id"});
+        // }
         else if(!course_duration){
             return res.status(404).json({message: "invalid course duration"});
         }
         else if(!day_number){
             return res.status(404).json({message: "invalid day number"});
         }
-        else if(!course_date){
-            return res.status(404).json({message: "invalid course date"});
-        }
+        // else if(!course_date){
+        //     return res.status(404).json({message: "invalid course date"});
+        // }
         else if(!createdBy){
             return res.status(404).json({message: "invalid createdBY"});
         }
 
-        const courseType = await getCourseTypeServices(course_type_id);
+        // const courseType = await getCourseTypeServices(course_type_id);
 
-        if(courseType==null){
-            return res.status(404).json({message: "no such course_type id exists"});
-        }
+        // if(courseType==null){
+        //     return res.status(404).json({message: "no such course_type id exists"});
+        // }
 
-        const newCourse = await createCourseServices(course_name, course_duration, course_type_id, course_link, day_number,course_date, createdBy);
+        const newCourse = await createCourseServices(course_name, course_duration, course_type, day_number, createdBy);
 
         if(newCourse==null){
             return res.status(400).json({message: "couldn't create table"});
--- types/modelTypes/courses.ts
+++ types/modelTypes/courses.ts
@@ -3,10 +3,10 @@ import {Model} from 'sequelize';
 class Courses extends Model{
     public course_id!: number;
     public day_number !: number;
-    public course_date !: Date;
+    // public course_date !: Date;
     public course_name!: string;
-    public course_type_id !: number;
-    public course_link?: string;
+    public course_type !: string;
+    // public course_link?: string;
     public course_duration?: string;
     public createdAt ?: Date;
     public createdBy ?: number;
