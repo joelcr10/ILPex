ffcca6f9ce483a1e0c602637681b1975bff63c47
Nebil V <nebilvxavier@gmail.com>
2024-02-27 14:04:37 +0530

    fix:getBatchDetails

--- src/controllers/l_and_d/getBatchDetailsController.ts
+++ src/controllers/l_and_d/getBatchDetailsController.ts
@@ -2,26 +2,33 @@ import { Op,Sequelize } from "sequelize";
 import {Router, Request, Response} from "express";
 import Batches from '../../models/batches';
 import batchDetailsServices from "../../services/l_and_d_services/batchDetailsServices";
+import getTraineesCount from "../../services/l_and_d_Services/getTraineesCount";
 
 const getBatchDetails=async(req:Request,res:Response):Promise<
 Response<
   any,
-  Record<string,| { message: string }|{batch_details:object}> 
+  Record<string,| { message: string }> 
 >>=>{
     try{
         const batch_id :number=parseInt(req.query.batch_id as string);
         
         if(!batch_id){
-            return res.status(400).json({message:"Batch ID is required."});
-        }
+            return res.status(400).json({message:"Invalid Batch ID"});
+        }  
         
         ////Call the service function to get batches details
         const batch_details= await batchDetailsServices(batch_id);
 
+        const noOfTrainees=await getTraineesCount(batch_id);
+
+        if(noOfTrainees==0){
+            return res.status(404).json({message:"No Trainees Found in the Specified Batch"});
+        }
+
         if(batch_details==null){
             return res.status(404).json({message:"Batch Not Found"});
         }
-        return res.status(200).json({batch_details:batch_details});
+        return res.status(200).json({batch_details,noOfTrainees});
         
     }
     catch(err){
