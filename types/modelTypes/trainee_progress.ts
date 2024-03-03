ce42f4285760fb4f6f6f1b6af7edbee8f66d3c8b
joelcr10 <joelcraju@gmail.com>
2024-03-03 16:08:11 +0530

    fix: added batch_id to trainee_progress

--- types/modelTypes/trainee_progress.ts
+++ types/modelTypes/trainee_progress.ts
@@ -4,7 +4,7 @@ class Trainee_Progress extends Model{
     public progress_id? : number;
     public trainee_id! : number;
     public course_id! : string;
-   
+    public batch_id!: number;
     public day_number!: number;
     public completion_status! : string;
     public createdAt ?: Date;
