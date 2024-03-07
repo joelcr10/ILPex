d7e851c50e0262ee945a71755a63f82bf0407311
ThimnaRaphel <thimnaraphel12@gmail.com>
2024-03-07 08:15:33 +0530

    pulling from testing

--- src/controllers/l_and_d/batchDayWiseProgressController.ts
+++ src/controllers/l_and_d/batchDayWiseProgressController.ts
@@ -3,7 +3,6 @@ import findBatchDayWiseProgressService from "../../services/l_and_d_Services/bat
 import findBatch from "../../services/adminServices/findBatch";
 import getTraineesCount from "../../services/l_and_d_Services/getTraineesCount";
 import getDaywiseCourseServices from "../../services/TraineeServices/getDaywiseCourseServices";
-import getWorkingDaysServices from "../../services/l_and_d_Services/getWorkingDaysServices";
 import getBatchCurrentDay from "../../services/l_and_d_Services/batchDayWiseProgress/getDayCountService";
 
 type progressData = {
@@ -12,7 +11,6 @@ type progressData = {
 let progressData : progressData ={};
 const getBatchDayWiseProgress=async(req:Request,res:Response)=>{
     try{
-        let currentDate = new Date();
         const batch_id :number=parseInt(req.params.batch_id as string);
 
         if(!batch_id){
@@ -24,38 +22,24 @@ const getBatchDayWiseProgress=async(req:Request,res:Response)=>{
                 return res.status(404).json({error : "Invalid batch_id"});
             }
             else{
-                // const dateArray:Date[] = getWorkingDaysServices(batch_found.start_date,currentDate);
-                // console.log("The date array",dateArray)
-                // if(!dateArray){
-                //     return res.status(404).json({error : "No date array"});
-                // }
-                // else{
-                    const totalDays : any = getBatchCurrentDay(batch_id);
-                    console.log("The total days are",totalDays);
-                    if(totalDays){
-                        const currentDay = totalDays[0];
-                        console.log(currentDay);
+                    const totalDays : any = await getBatchCurrentDay(batch_id);
+                    if(totalDays!=0){
+                        const currentDay = totalDays[0].dataValues.day_number;
                         const trainee_count = await getTraineesCount(batch_id);
-                        console.log("The trainee count is",trainee_count);
                         for (let i=1;i <= currentDay;i++){
                             const batchDayWiseProgressCount = await findBatchDayWiseProgressService(batch_id,i);
-                            console.log("batchDayWiseProgressCount",batchDayWiseProgressCount);
                             const dayWiseCourses = await getDaywiseCourseServices(i);
-                            console.log("dayWiseCourses",dayWiseCourses);
                             const dayWiseCourses_count = (dayWiseCourses).length;
-                            console.log("dayWiseCourses_count",dayWiseCourses_count);
                             const total_courses = trainee_count*dayWiseCourses_count;
-                            console.log("total_courses",total_courses);
                             const progress:number = (batchDayWiseProgressCount/(total_courses)) * 100;
                             if(isNaN(progress)){
                                 progress==0;
                             }
-                            console.log("Progress is", progress);
                             progressData[i] = progress;
                         }
                     }
                 else{
-                    return res.status(404).json({ error : "There is some issue"})
+                    return res.status(404).json({ error : "There is no progress for the batch"})
                 }
             }
             return res.status(200).json({data : {progressData}});
