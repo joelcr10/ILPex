eda2976a35bd70e88a21800b37bad9614146f73e
Sreejaya <itsmesreejaya@gmail.com>
2024-03-11 14:58:55 +0530

    fix:reset passwordAPI fixed

--- src/services/authentication/sendOTPByEmailService.ts
+++ src/services/authentication/sendOTPByEmailService.ts
@@ -1,4 +1,3 @@
-// emailService.ts
 import nodemailer from "nodemailer";
 import Users from "../../models/users";
 
@@ -7,43 +6,45 @@ export const otpStorage: { [email: string]: string } = {};
 const generateOTP = () => (1000 + Math.floor(Math.random() * 9000)).toString();
 
 export const sendOTPByEmail = async (email: string) => {
-
   const transporter = nodemailer.createTransport({
-    host: "smtp.gmail.com",  //smtp server of gmail
+    host: "smtp.gmail.com",
     port: 465,
     secure: true,
     auth: {
-      // TODO: replace `user` and `pass` values from <https://forwardemail.net>
       user: "joelcrajudeveloper@gmail.com",
-      pass: "xkrv ohcg pxjj sxah", //app password in 2 step authenticaion
+      pass: "xkrv ohcg pxjj sxah",
     },
   });
-
 
   const userFound = await Users.findOne({
     where: { email: email },
   });
-  
-  if(userFound){
+
+  if (userFound) {
     const otp = generateOTP();
 
-    const testMail = async (transporter:any, email: string) =>{
+    const testMail = async (transporter: any, email: string) => {
       const info = await transporter.sendMail({
-          from: '"ILPex"', // sender address
-          to: email, // list of receivers
-          subject: "Verification Code", // Subject line
-          // text: "", // plain text body
-          html: `
-                  <b>Hello ${userFound.user_name}</b>
-                  <br>
-                  <p>Your verification code is: ${otp}</p>                
-                `, // html body
-        });
-    
-        return info;
-    
-    }
-  
+        from: '"ILPex" <joelcrajudeveloper@gmail.com>', // sender address
+        to: email,
+        subject: "Verification Code",
+        html: `
+        <b>Hello ${userFound.user_name},</b>
+        <br>
+        <p>Thank you for using ILPex. Your verification code is:</p>
+        <h2>${otp}</h2>
+        <p>Please use this code to complete the verification process.</p>
+        <br>
+        <p>If you didn't request this code, please ignore this email.</p>
+        <br>
+        <p>Best regards,</p>
+        <p>The ILPex Team</p>
+        `,
+      });
+
+      return info;
+    };
+
     try {
       const test = await testMail(transporter, email);
       otpStorage[email] = otp;
@@ -52,14 +53,7 @@ export const sendOTPByEmail = async (email: string) => {
       console.error("Error sending OTP:", error);
       return false;
     }
-  } else{
-     return false;
+  } else {
+    return false;
   }
-  
 };
-
-
-
-
-
-
