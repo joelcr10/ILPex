cb8a1e20aa20043eb705701c2f4a88286eb3fe3e
Nebil V <nebilvxavier@gmail.com>
2024-03-26 14:27:49 +0530

    merge commit

--- dist/src/services/l_and_d_Services/sendMail.js
+++ dist/src/services/l_and_d_Services/sendMail.js
@@ -46,8 +46,6 @@ const sendMail = (receiverMail, username, day_number) => __awaiter(void 0, void 
             pass: process.env.NOTIFICATION_PASS, //app password in 2 step authenticaion
         },
     });
-
-    
     const test = yield testMail(transporter, receiverMail, username, day_number);
     return "success";
 });
