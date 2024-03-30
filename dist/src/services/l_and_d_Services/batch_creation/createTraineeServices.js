7b459300353743733d4616399ec507b7a5a1b013
ThimnaRaphel <thimnaraphel12@gmail.com>
2024-03-30 18:16:33 +0530

    pulling from testing

--- dist/src/services/l_and_d_Services/batch_creation/createTraineeServices.js
+++ dist/src/services/l_and_d_Services/batch_creation/createTraineeServices.js
@@ -16,7 +16,7 @@ const trainees_1 = __importDefault(require("../../../models/trainees"));
 const createTraineeServices = (user_id, batch_id, userID) => __awaiter(void 0, void 0, void 0, function* () {
     const createTrainee = yield trainees_1.default.create({
         user_id: user_id,
-        batch_id: batch_id, // Use the batch_id from the createdBatch
+        batch_id: batch_id,
         isActive: true,
         createdBy: userID,
         modifiedBy: userID
