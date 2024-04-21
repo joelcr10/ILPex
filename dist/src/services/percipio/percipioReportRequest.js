0d14061c9581552a20b0b88b1eeed3119eebbe7c
ELENAMARIAVARGHESE <elenamariavarghese@gmail.com>
2024-04-21 13:23:11 +0530

    feat : added no of attempts left in get assessments api

--- dist/src/services/percipio/percipioReportRequest.js
+++ dist/src/services/percipio/percipioReportRequest.js
@@ -16,31 +16,31 @@ const axios_1 = __importDefault(require("axios"));
 const percipioReportRequest = () => __awaiter(void 0, void 0, void 0, function* () {
     const currentDate = new Date();
     console.log("current date---->", currentDate);
-    const apiUrl = "https://api.percipio.com/reporting/v1/organizations/7d980d20-af30-4dde-a9d7-9632c96541b9/report-requests/learning-activity";
+    const apiUrl = 'https://api.percipio.com/reporting/v1/organizations/7d980d20-af30-4dde-a9d7-9632c96541b9/report-requests/learning-activity';
     const bearerToken = process.env.PERCIPIO_TOKEN;
     const reqBody = {
-        start: "2024-03-04T00:00:00Z",
-        end: "2024-04-02T23:59:59Z",
-        audience: "ALL",
-        contentType: "Course,Linked Content,Scheduled Content,Assessment",
-        csvPreferences: {
-            header: true,
-            rowDelimiter: "\\n",
-            columnDelimiter: ",",
-            headerForNoRecords: false,
+        "start": "2024-04-05T00:00:00Z",
+        "end": currentDate,
+        "audience": "ALL",
+        "contentType": "Course,Linked Content,Scheduled Content,Assessment",
+        "csvPreferences": {
+            "header": true,
+            "rowDelimiter": "\\n",
+            "columnDelimiter": ",",
+            "headerForNoRecords": false
         },
-        sort: {
-            field: "lastAccessDate",
-            order: "desc",
+        "sort": {
+            "field": "lastAccessDate",
+            "order": "desc"
         },
-        isFileRequiredInSftp: false,
-        formatType: "JSON",
+        "isFileRequiredInSftp": false,
+        "formatType": "JSON"
     };
     try {
         const response = yield axios_1.default.post(apiUrl, reqBody, {
             headers: {
-                Authorization: `Bearer ${bearerToken}`,
-                "Content-Type": "application/json", // Adjust the content type based on your API requirements
+                'Authorization': `Bearer ${bearerToken}`,
+                'Content-Type': 'application/json', // Adjust the content type based on your API requirements
             },
         });
         return response.data.id;
