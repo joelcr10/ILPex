934aa4648b3957a3932d6670b7712c351aeb8e8c
ThimnaRaphel <thimnaraphel12@gmail.com>
2024-04-24 17:39:58 +0530

    feat:added generate batch report day wise api

---
+++ src/controllers/l_and_d/generateBatchDayWiseReport.ts
@@ -0,0 +1,75 @@
+import { Request, Response } from "express";
+import findTraineesOfABatchServices from "../../services/l_and_d_Services/trainee_analysis/findTraineesOfABatchServices";
+import findUserId from "../../services/adminServices/findUserId";
+import getCourseCollectionOfABatchByBatchIDServices from "./getCourseCollectionOfABatchByBatchIDServices";
+import Trainee_Progress from "../../models/trainee_progress";
+import getDaywiseCourseServices from "../../services/TraineeServices/getDaywiseCourseServices";
+
+const generateBatchReportDayWiseController = async (
+  req: Request,
+  res: Response
+) => {
+  try {
+    const batch_id: number = parseInt(req.params.batch_id as string);
+    const day_id: number = parseInt(req.params.day_id as string);
+    console.log("batch_id------------", batch_id);
+    console.log("day_id-------------------->", day_id);
+    if (!batch_id || !day_id) {
+      return res.json({ error: "Please provide the batch_id and day_id" });
+    } else {
+      const coursesCollectionIdOfABatch =
+        await getCourseCollectionOfABatchByBatchIDServices(batch_id);
+      const coursesOfABatch = await getDaywiseCourseServices(
+        day_id,
+        coursesCollectionIdOfABatch.course_set_id
+      );
+      const trainees = await findTraineesOfABatchServices(batch_id);
+      if (trainees) {
+        const traineesData = [];
+        for (const trainee of trainees) {
+          let watched = 0;
+          const user = await findUserId(trainee.user_id);
+          const userName = user.user_name;
+          const traineeData = {
+            trainee: userName,
+            courses: [],
+            totalCourses: coursesOfABatch.length,
+            watchedCourses: watched,
+          };
+          for (const course of coursesOfABatch) {
+            const traineeReport = await Trainee_Progress.findOne({
+              where: {
+                trainee_id: trainee.trainee_id,
+                course_id: course.course_id,
+              },
+            });
+            if (traineeReport) {
+              traineeData.courses.push({
+                course: course.course_name,
+                watchStatus: "Yes",
+                duration: course.course_duration,
+                watchTime: traineeReport.duration,
+              });
+              watched += 1;
+            } else {
+              traineeData.courses.push({
+                course: course.course_name,
+                watchStatus: "No",
+                duration: course.course_duration,
+                watchTime: "NA",
+              });
+            }
+          }
+
+          traineesData.push(traineeData);
+        }
+        res.status(200).json(traineesData);
+      }
+    }
+  } catch (error) {
+    console.log("Error while generating the report", error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+};
+
+export default generateBatchReportDayWiseController;
--- src/routes/l_and_d_routes.ts
+++ src/routes/l_and_d_routes.ts
@@ -1,7 +1,8 @@
 import { Router, Request, Response } from "express";
-import getTrainess from '../controllers/l_and_d/getTraineesController';
+import getTrainess from "../controllers/l_and_d/getTraineesController";
 import createAssessmentController from "../controllers/l_and_d/createAssessmentsController";
-import updateAssessmentController from "../controllers/l_and_d/updateAssessmentsController";import getBatchDetails from "../controllers/l_and_d/getBatchDetailsController";
+import updateAssessmentController from "../controllers/l_and_d/updateAssessmentsController";
+import getBatchDetails from "../controllers/l_and_d/getBatchDetailsController";
 import getAllAsssessment from "../controllers/l_and_d/getAllAssessmentsController";
 import batchCourseAnalysisController from "../controllers/l_and_d/batchCourseAnalysisController";
 import batchAverage from "../controllers/l_and_d/batchAverageScore";
@@ -14,8 +15,8 @@ import getAllBatches from "../controllers/l_and_d/getAllBatchesController";
 import getIncompleteTraineeList from "../controllers/l_and_d/getIncompleteTraineeList";
 import sendMailController from "../controllers/l_and_d/sendMailController";
 import verifyLoginJWT from "../middlewares/verifyLoginJWT";
-import multer from 'multer';
-import fs from 'fs';
+import multer from "multer";
+import fs from "fs";
 import batchPercipioController from "../controllers/l_and_d/batchPercipioController";
 import getPercipioAssessmentController from "../controllers/l_and_d/getPercipioAssessmentScoresController";
 import batchWatchTimeReportController from "../controllers/l_and_d/batchWatchTimeReportController";
@@ -28,144 +29,239 @@ import getCompleteTraineeList from "../controllers/l_and_d/getDayWiseCompleteTra
 import getBatchwiseCompleteTraineesList from "../controllers/l_and_d/getBatchWiseCompleteTraineesList";
 import deactivateCourseController from "../controllers/l_and_d/deactivateCoursesController";
 import generateBatchReportController from "../controllers/l_and_d/generateBatchReportController";
+import generateBatchReportDayWiseController from "../controllers/l_and_d/generateBatchDayWiseReport";
 
-//Multer DiskStorage Config 
+//Multer DiskStorage Config
 const storage = multer.diskStorage({
-    destination : function(req, file, cb) {
-        let dir = `D:\ILPex\TemporaryFileStorage`;
+  destination: function (req, file, cb) {
+    let dir = `D:\ILPex\TemporaryFileStorage`;
 
-        fs.access(dir, function(error)
-        {
-            if(error)
-            {
-                console.log('Directory does not Exist');
-                return fs.mkdir(dir, error => cb(error, dir));
-            }
-            else
-            {
-                console.log('Directory Exists');
-                return cb(null, dir);
-            }
-        });
-    },
-    filename : function(req, file, cb) {
-        cb(null, Date.now() + '-' + file.originalname);
-    }
+    fs.access(dir, function (error) {
+      if (error) {
+        console.log("Directory does not Exist");
+        return fs.mkdir(dir, (error) => cb(error, dir));
+      } else {
+        console.log("Directory Exists");
+        return cb(null, dir);
+      }
+    });
+  },
+  filename: function (req, file, cb) {
+    cb(null, Date.now() + "-" + file.originalname);
+  },
 });
 
-const uploadFiles = multer({storage : storage});
+const uploadFiles = multer({ storage: storage });
 
 const router = Router();
 
-router.get('/batch/currentDayUpdate',verifyLoginJWT,async (req: Request, res: Response) =>{
-    updateCurrentDayController(req,res);
-})
+router.get(
+  "/batch/currentDayUpdate",
+  verifyLoginJWT,
+  async (req: Request, res: Response) => {
+    updateCurrentDayController(req, res);
+  }
+);
 
-router.get("/trainee",verifyLoginJWT, async (req: Request, res: Response) => {
-    getTrainess(req, res);
+router.get("/trainee", verifyLoginJWT, async (req: Request, res: Response) => {
+  getTrainess(req, res);
 });
 
-router.post('/assessment',verifyLoginJWT, uploadFiles.single('file'),async (req, res, next)=>{
+router.post(
+  "/assessment",
+  verifyLoginJWT,
+  uploadFiles.single("file"),
+  async (req, res, next) => {
     const file = req.file;
     if (!file) {
-        const error = new Error("Please upload a file");
-        return next(error);
+      const error = new Error("Please upload a file");
+      return next(error);
     }
-    
+
     // Pass the file to createBatchController
     req.file = file;
-    createAssessmentController(req,res);
-});
-router.patch('/assessment',verifyLoginJWT,async (req:Request,res:Response)=>{
-    updateAssessmentController(req,res);
-});
-router.get('/batch/:batch_id/progress',verifyLoginJWT,async(req:Request,res:Response)=>{
-    batchDayWiseProgressController(req,res);
-})
-router.get("/batch/:batch_id",verifyLoginJWT, async (req: Request, res: Response) => {
+    createAssessmentController(req, res);
+  }
+);
+router.patch(
+  "/assessment",
+  verifyLoginJWT,
+  async (req: Request, res: Response) => {
+    updateAssessmentController(req, res);
+  }
+);
+router.get(
+  "/batch/:batch_id/progress",
+  verifyLoginJWT,
+  async (req: Request, res: Response) => {
+    batchDayWiseProgressController(req, res);
+  }
+);
+router.get(
+  "/batch/:batch_id",
+  verifyLoginJWT,
+  async (req: Request, res: Response) => {
     getBatchDetails(req, res);
+  }
+);
+
+router.get("/batch", verifyLoginJWT, async (req: Request, res: Response) => {
+  getAllBatches(req, res);
 });
 
-router.get("/batch",verifyLoginJWT, async (req: Request, res: Response) => {
-    getAllBatches(req, res);
-});
-
-router.get("/assessment/:assessment_id",verifyLoginJWT, async (req: Request, res: Response) => {
+router.get(
+  "/assessment/:assessment_id",
+  verifyLoginJWT,
+  async (req: Request, res: Response) => {
     getAssessmentDetails(req, res);
-});
+  }
+);
 
-router.get("/assessment",verifyLoginJWT, async (req: Request, res: Response) => {
+router.get(
+  "/assessment",
+  verifyLoginJWT,
+  async (req: Request, res: Response) => {
     getAllAsssessment(req, res);
-});
-router.get('/batchAvg/:id',verifyLoginJWT,async (req:Request,res:Response)=>{
-    batchAverage(req,res);
-});
-router.get('/percipioAssesmentAvg/:id',verifyLoginJWT,async (req:Request,res:Response)=>{
-    percipioAssesmentAverage(req,res);
-});
-router.post('/pending/day/mail',verifyLoginJWT, async(req: Request, res: Response) =>{
-    sendMailController(req,res);
-});
+  }
+);
+router.get(
+  "/batchAvg/:id",
+  verifyLoginJWT,
+  async (req: Request, res: Response) => {
+    batchAverage(req, res);
+  }
+);
+router.get(
+  "/percipioAssesmentAvg/:id",
+  verifyLoginJWT,
+  async (req: Request, res: Response) => {
+    percipioAssesmentAverage(req, res);
+  }
+);
+router.post(
+  "/pending/day/mail",
+  verifyLoginJWT,
+  async (req: Request, res: Response) => {
+    sendMailController(req, res);
+  }
+);
 
-router.get('/analysis/:batch_id',verifyLoginJWT, async(req : Request, res : Response) => {
+router.get(
+  "/analysis/:batch_id",
+  verifyLoginJWT,
+  async (req: Request, res: Response) => {
     batchCourseAnalysisController(req, res);
-});
+  }
+);
 
-router.get('/trainee/:trainee_id/scores',verifyLoginJWT, async(req: Request, res: Response) =>{
-    traineeScore(req,res);
-})
+router.get(
+  "/trainee/:trainee_id/scores",
+  verifyLoginJWT,
+  async (req: Request, res: Response) => {
+    traineeScore(req, res);
+  }
+);
 
-router.get('/analysis/:batch_id/:day_id',verifyLoginJWT, async(req : Request, res : Response) => {
+router.get(
+  "/analysis/:batch_id/:day_id",
+  verifyLoginJWT,
+  async (req: Request, res: Response) => {
     batchDayWiseCourseAnalysisController(req, res);
-})
+  }
+);
 
-router.get("/batch/:batch_id/pending/day/:id",verifyLoginJWT, async (req: Request, res: Response) => {
+router.get(
+  "/batch/:batch_id/pending/day/:id",
+  verifyLoginJWT,
+  async (req: Request, res: Response) => {
     getIncompleteTraineeList(req, res);
+  }
+);
+
+router.post("/batch/percipio", async (req: Request, res: Response) => {
+  batchPercipioController(req, res);
 });
 
-router.post("/batch/percipio", async (req: Request, res: Response) =>{
-    batchPercipioController(req,res);
-})
+router.get(
+  "/trainee/:trainee_id/percipio/assessment",
+  verifyLoginJWT,
+  async (req: Request, res: Response) => {
+    getPercipioAssessmentController(req, res);
+  }
+);
 
-router.get("/trainee/:trainee_id/percipio/assessment",verifyLoginJWT,async (req:Request,res:Response) =>{
-    getPercipioAssessmentController(req,res);
-})
-
-router.get('/batch/:batch_id/watchtime', async(req : Request, res : Response) => {
+router.get(
+  "/batch/:batch_id/watchtime",
+  async (req: Request, res: Response) => {
     batchWatchTimeReportController(req, res);
-})
+  }
+);
 
-router.get("/batch/:batch_id/incompleteTrainees/:day_id",verifyLoginJWT, async (req: Request, res: Response) => {
+router.get(
+  "/batch/:batch_id/incompleteTrainees/:day_id",
+  verifyLoginJWT,
+  async (req: Request, res: Response) => {
     getIncompleteTraineeListForDay(req, res);
-});
+  }
+);
 
-router.get('/batch/:batch_id/incompleteTrainees/day/:day_id', verifyLoginJWT, async (req: Request, res: Response)=> {
+router.get(
+  "/batch/:batch_id/incompleteTrainees/day/:day_id",
+  verifyLoginJWT,
+  async (req: Request, res: Response) => {
     batchDayWiseIncompleteTraineeListController(req, res);
-});
+  }
+);
 
-router.post('/assessment/mail',verifyLoginJWT, async(req: Request, res: Response) =>{
-    sendAssessmentMailController(req,res);
-});
+router.post(
+  "/assessment/mail",
+  verifyLoginJWT,
+  async (req: Request, res: Response) => {
+    sendAssessmentMailController(req, res);
+  }
+);
 
-router.get('/batch/:batch_id/completeTrainees/:id',verifyLoginJWT, async(req: Request, res: Response) =>{
+router.get(
+  "/batch/:batch_id/completeTrainees/:id",
+  verifyLoginJWT,
+  async (req: Request, res: Response) => {
     getCompleteTraineeList(req, res);
-});
+  }
+);
 
-router.get('/batch/:batch_id/completeTrainees/currentDay/:day_id',verifyLoginJWT, async(req: Request, res: Response) =>{
+router.get(
+  "/batch/:batch_id/completeTrainees/currentDay/:day_id",
+  verifyLoginJWT,
+  async (req: Request, res: Response) => {
     getBatchwiseCompleteTraineesList(req, res);
-});
+  }
+);
 
-router.get('/batch/:batch_id/completeTrainees/currentDay/:day_id',verifyLoginJWT, async(req: Request, res: Response) =>{
+router.get(
+  "/batch/:batch_id/completeTrainees/currentDay/:day_id",
+  verifyLoginJWT,
+  async (req: Request, res: Response) => {
     getBatchwiseCompleteTraineesList(req, res);
-});
+  }
+);
 
-router.post('/course/deactivate', verifyLoginJWT, async(req : Request, res : Response) => {
+router.post(
+  "/course/deactivate",
+  verifyLoginJWT,
+  async (req: Request, res: Response) => {
     deactivateCourseController(req, res);
+  }
+);
+
+router.get("/batch/:batch_id/report", async (req: Request, res: Response) => {
+  generateBatchReportController(req, res);
 });
 
-router.get('/batch/:batch_id/report',async(req : Request, res : Response) => {
-    generateBatchReportController(req,res);
-})
-
+router.get(
+  "/batch/:batch_id/report/:day_id",
+  async (req: Request, res: Response) => {
+    generateBatchReportDayWiseController(req, res);
+  }
+);
 
 export default router;
