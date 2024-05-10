b97b47b86b74f089fd6310cb677214a0f1564549
Nigin N Manayil <niginnmanayil10@gmail.com>
2024-05-10 17:49:49 +0530

    fix : fixed findTraineeProgressOfADay

--- src/services/l_and_d_Services/findTraineeProgressOfADay.ts
+++ src/services/l_and_d_Services/findTraineeProgressOfADay.ts
@@ -5,6 +5,8 @@ const findTraineeProgressOfADay = async (
   current_day: number
 ) => {
   const progressCount = await Trainee_Progress.count({
+    distinct: true,
+    col: "course_id",
     where: { trainee_id: trainee_id, day_number: current_day },
   });
   return progressCount;
