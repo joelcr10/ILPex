b11f1afedb42ce2efa2f3b6f9af4784f4b0a9b23
Nigin N Manayil <niginnmanayil10@gmail.com>
2024-05-10 17:39:54 +0530

    fix : Corrected batch Course Analysis Controller

---
+++ src/services/l_and_d_Services/findTraineeProgressOfADay.ts
@@ -0,0 +1,13 @@
+import Trainee_Progress from "../../models/trainee_progress";
+
+const findTraineeProgressOfADay = async (
+  trainee_id: number,
+  current_day: number
+) => {
+  const progressCount = await Trainee_Progress.count({
+    where: { trainee_id: trainee_id, day_number: current_day },
+  });
+  return progressCount;
+};
+
+export default findTraineeProgressOfADay;
