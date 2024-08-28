bea838b06c0b394766c6f3438a92e0c515191570
Ashik George <ashikgeorgec@gmail.com>
2024-08-28 16:25:42 +0530

    saturday check

--- src/services/adminServices/updateBatchName.ts
+++ src/services/adminServices/updateBatchName.ts
@@ -1,6 +1,7 @@
-const updateBatchName =async(batch:any,BatchName:string)=>{
+const updateBatchName =async(batch:any,BatchName:string,include_saturdays : boolean)=>{
     
     await batch.update({batch_name:BatchName})
+    await batch.update({include_saturdays:include_saturdays})
     return batch;
 }
 export  default updateBatchName;
