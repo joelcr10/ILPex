9d3125d7236d6cd0bdf859355d5f04e56fb2d4c6
joelcr10 <joelcraju@gmail.com>
2024-12-06 10:44:01 +0530

    deleted dist

--- dist/types/modelTypes/assessment_batch_allocation.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const sequelize_1 = require("sequelize");
-class Assessment_Batch_Allocation extends sequelize_1.Model {
-}
-exports.default = Assessment_Batch_Allocation;
--- dist/types/modelTypes/results.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const sequelize_1 = require("sequelize");
-class Results extends sequelize_1.Model {
-}
-exports.default = Results;
